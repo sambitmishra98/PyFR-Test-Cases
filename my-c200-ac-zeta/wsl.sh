2613e989c96f63d51dd40277ecb65edd3d3cf556
Sambit Mishra <sambit98@tamu.edu>
2022-11-13 19:02:19 -0600

    name change

---
+++ my-c200-ac-zeta/wsl.sh
@@ -0,0 +1,5 @@
+. /home/sambit98/pyfr-installation/pyfr-venv/bin/activate
+
+/home/sambit98/pyfr-installation/pyfr-venv/bin/pyfr restart -b openmp 2d-inc-cylinder.pyfrm 2d-inc-cylinder-75.00.pyfrs 2d-inc-cylinder.ini
+
+
