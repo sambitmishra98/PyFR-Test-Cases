2c10a6fc7873f256c3a506c21b28f2c614909cd1
entropy <sambit98@tamu.edu>
2023-08-09 12:31:28 -0500

    Compare changes in dt.

--- 2d-euler-vortex/plot-compare-pseudo.py
+++ 2d-euler-vortex/plot-compare-pseudo.py
@@ -2,22 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-#2x1
-fig, ax = plt.subplots(2, 1, figsize=(10, 5))
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
 
-df_come_im_int = pd.read_csv('compressible-implicit-int.csv')
 
-df_come_im_psu = pd.read_csv('compressible-implicit-.csv')
+df_come_im_dt  = pd.read_csv('pseudostats.csv')
+df_come_im_dt2 = pd.read_csv('pseudostats_half-dt.csv')
 
-ax[0].plot(df_come_im_int['t'], df_come_im_int['int-E'], label='compressible-implicit-PR1')
-ax[0].set_xlabel('Time (s)')
-ax[0].set_ylabel('Internal Energy (J)')
-ax[0].legend()
-
-ax[1].plot(df_come_im_psu['t'], df_come_im_psu['E'], label='compressible-implicit-PR1')
-ax[1].set_xlabel('Time (s)')
-ax[1].set_ylabel('Residual')
-ax[1].legend()
+ax.semilogy(df_come_im_dt['n'], df_come_im_dt['E'],  label=r'$\rho$')
+ax.semilogy(df_come_im_dt2['n'], df_come_im_dt2['E'],  label=r'$\rho$', linestyle='--')
 
 plt.tight_layout()
 plt.savefig('pseudo-res.png')
