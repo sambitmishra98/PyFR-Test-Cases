f5e924214323ef8659297b811906125dd2f85afb
entropy <sambit98@tamu.edu>
2023-08-09 14:45:08 -0500

    Remove png files and python file from gitignore

--- 2d-euler-vortex/plot-compare-pseudo.py
+++ 2d-euler-vortex/plot-compare-pseudo.py
@@ -2,14 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+fig, (ax, ax2) = plt.subplots(2, 1, figsize=(10, 5))
 
 
-df_come_im_dt  = pd.read_csv('pseudostats.csv')
-df_come_im_dt2 = pd.read_csv('pseudostats_half-dt.csv')
+df1  = pd.read_csv('esdirk23-pseudostats.csv')
+df2  = pd.read_csv('sdirk33-pseudostats.csv')
+df3  = pd.read_csv('sdirk43-pseudostats.csv')
+df4  = pd.read_csv('esdirk35-pseudostats.csv')
 
-ax.semilogy(df_come_im_dt['n'], df_come_im_dt['E'],  label=r'$\rho$')
-ax.semilogy(df_come_im_dt2['n'], df_come_im_dt2['E'],  label=r'$\rho$', linestyle='--')
+# Plot only the fifth entry
+
+ax.semilogy(df2['t'][::15], df2['E'][::15],  label=r'$SDIRK33$')
+ax.semilogy(df3['t'][::15], df3['E'][::15],  label=r'$SDIRK43$')
+ax.semilogy(df1['t'][10::11], df1['E'][10::11],  label=r'$ESDIRK23$')
+ax.semilogy(df4['t'][20::21], df4['E'][20::21],  label=r'$ESDIRK35$')
+
+ax.set_xlabel(r'$t$')
+
+ax2.semilogy(df2['n'][:15], df2['E'][:15],  label=r'$SDIRK33$')
+ax2.semilogy(df3['n'][:15], df3['E'][:15],  label=r'$SDIRK43$')
+ax2.semilogy(df1['n'][:11], df1['E'][:11],  label=r'$ESDIRK23$')
+ax2.semilogy(df4['n'][:21], df4['E'][:21],  label=r'$ESDIRK35$')
+
+# circle the last point with the same color as the line
+ax2.scatter(df2['n'][14], df2['E'][14], s=100, facecolors='none', edgecolors='C0')
+ax2.scatter(df3['n'][14], df3['E'][14], s=100, facecolors='none', edgecolors='C1')
+ax2.scatter(df1['n'][10], df1['E'][10], s=100, facecolors='none', edgecolors='C2')
+ax2.scatter(df4['n'][20], df4['E'][20], s=100, facecolors='none', edgecolors='C3')
+
+
+
+
+ax2.set_xlabel(r'$n$')
+ax2.legend(loc='best')
 
 plt.tight_layout()
 plt.savefig('pseudo-res.png')
--- 2d-inc-cylinder/plot-compare-int.py
+++ 2d-inc-cylinder/plot-compare-int.py
@@ -5,18 +5,18 @@ import pandas as pd
 fig, ax = plt.subplots(1, 1, figsize=(5, 3))
 
 
-df1  = pd.read_csv('esdirk23-int.csv')
-df2  = pd.read_csv('sdirk33-int.csv')
-df3  = pd.read_csv('sdirk43-int.csv')
-df4  = pd.read_csv('esdirk35-int.csv')
+df1  = pd.read_csv('int_esdirk23.csv')
+df2  = pd.read_csv('int_sdirk33.csv')
+#df3  = pd.read_csv('int_sdirk43.csv')
+df4  = pd.read_csv('int_esdirk35.csv')
 
 
 # Plot only the fifth entry
 
 ax.plot(df1['t'], df1['int-E'], '--', label=r'$ESDIRK23$', linewidth=3)
 ax.plot(df2['t'], df2['int-E'], label=r'$SDIRK33$', linewidth=2)
-ax.plot(df3['t'], df3['int-E'], '--', label=r'$SDIRK43$', linewidth=3)
-ax.plot(df4['t'], df3['int-E'], label=r'$ESDIRK35$', linewidth=2)
+#ax.plot(df3['t'], df3['int-E'], '--', label=r'$SDIRK43$', linewidth=3)
+ax.plot(df4['t'], df4['int-E'], label=r'$ESDIRK35$', linewidth=2)
 
 ax.set_xlabel(r'$n$')
 ax.legend(loc='best')
